a4ff6bf96121df75595ca90ea786ba1e523f98f0
Jeanne Franco <44760301+francojra@users.noreply.github.com>
2022-09-17 22:41:28 -0300

    Add files via upload

---
+++ prettydoc_rmarkdown_html.R
@@ -0,0 +1,25 @@
+
+# Personalizando seus relatórios R Markdown em HTML com o {prettydoc} ----------------------------------------------------------------------
+# Autoria do script: Jeanne Franco ---------------------------------------------------------------------------------------------------------
+# Data: 17/09/22 ---------------------------------------------------------------------------------------------------------------------------
+# Referência: https://blog.curso-r.com/posts/2022-02-24-prettydoc/ -------------------------------------------------------------------------
+
+# Sobre o pacote 'prettydoc' ---------------------------------------------------------------------------------------------------------------
+
+### O 'prettydoc' é um pacote que oferece alguns templates bem bonitos para
+### serem utilizados em relatórios HTML construídos com R Markdown. Nesse
+### tutorial, vamos mostrar como usar esses templates em seus relatórios.
+
+# Instalar o pacote ------------------------------------------------------------------------------------------------------------------------
+
+install.packages("prettydoc")
+library(prettydoc)
+
+# Próximas etapas --------------------------------------------------------------------------------------------------------------------------
+
+### 1 - Crie um arquivo RMarkdown;
+### 2 - Selecione a opção HTML como formato de saída, 
+### e então, clique em From Template.
+### 3 - Selecione a opção Lightweight and Pretty Document (HTML) e
+### então, clique em OK.
+
